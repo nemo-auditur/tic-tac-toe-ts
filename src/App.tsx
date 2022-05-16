9880b11f9ac371c8921cf8afe9b9be24b7ceb735
Clément Legal <clement@macbook-pro-de-clement.home>
2022-05-16 21:53:36 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App-header">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    text: string
+}
+
+const Header: React.FC<Props>= () => {
+    return(
+        <>
+        <div></div>
+        </>
+    )
+}
+
+export default Header
